56714e82a3f12f927d1317173dd59f8e5bc936d8
fakorede <abiolafakorede@gmail.com>
2018-12-26 15:53:14 +0100

    modified views

--- resources/views/admin/categories/index.blade.php
+++ resources/views/admin/categories/index.blade.php
@@ -21,7 +21,12 @@
                         </td>
                         <td>
                             <a href="{{ route('category.edit', [ 'id' => $category->id ]) }}" class="btn btn-xs btn-info">
-                                <span class="glphyicon glyphicon-pencil"></span>
+                                Edit
+                            </a>
+                        </td>
+                        <td>
+                            <a href="{{ route('category.delete', [ 'id' => $category->id ]) }}" class="btn btn-xs btn-danger">
+                                Delete
                             </a>
                         </td>
                     </tr>
