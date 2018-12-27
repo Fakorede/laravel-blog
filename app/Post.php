4c711072f0371ff716784c03bd8aada373d79936
fakorede <abiolafakorede@gmail.com>
2018-12-27 02:20:22 +0100

    mass assignment for posts

--- app/Post.php
+++ app/Post.php
@@ -6,6 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
+
+    // The attributes that are mass assignable
+    protected $fillable = [
+      'title', 'content', 'category_id', 'featured'
+  ];
+
     public function category() {
       return $this->belongsTo('App\Category');
     }
