e3dba99757a545767d5ceedc2cd22681824b31a9
fakorede <abiolafakorede@gmail.com>
2018-12-27 02:32:14 +0100

    implemented SoftDeletes on Post model and reran all migrations

--- app/Post.php
+++ app/Post.php
@@ -3,14 +3,21 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
 
 class Post extends Model
 {
+
+
+    use SoftDeletes;
 
     // The attributes that are mass assignable
     protected $fillable = [
       'title', 'content', 'category_id', 'featured'
   ];
+
+    protected $dates = ['deleted_at'];
 
     public function category() {
       return $this->belongsTo('App\Category');
