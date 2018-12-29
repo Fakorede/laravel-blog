2e6edb6df9039d66b620312820aa3c52b8341001
fakorede <abiolafakorede@gmail.com>
2018-12-29 19:23:10 +0100

    controller method for viewing all trashed posts

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -121,4 +121,10 @@ class PostsController extends Controller
 
         return redirect()->back();
     }
+
+    public function trashed() {
+        $posts = Post::onlyTrashed()->get();
+        
+        return view('admin.posts.trashed')->with('posts', $posts);
+    }
 }
