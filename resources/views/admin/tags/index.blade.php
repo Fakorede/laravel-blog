be14a764e7c02d7bd338c0acb30030a55ff8022d
fakorede <abiolafakorede@gmail.com>
2018-12-30 15:50:33 +0100

    created views for tag

---
+++ resources/views/admin/tags/index.blade.php
@@ -0,0 +1,53 @@
+@extends('layouts.app')
+
+@section('content')
+
+<div class="panel panel-default">
+
+    <div class="panel-heading">
+        TAGS
+    </div>
+    
+    <div class="panel-body">
+    
+        <table class="table table-hover">
+            <thead>
+                <th>Tag name</th>
+                <th>Edit Tag</th>
+                <th>Delete Tag</th>
+            </thead>
+            <tbody>
+                @if($tags->count() > 0)
+                    @foreach($tags as $tag)
+
+                        <tr>
+                            <td>
+                                {{ $tag->tag }}
+                            </td>
+                            <td>
+                                <a href="{{ route('tag.edit', [ 'id' => $tag->id ]) }}" class="btn btn-xs btn-info">
+                                    Edit
+                                </a>
+                            </td>
+                            <td>
+                                <a href="{{ route('tag.delete', [ 'id' => $tag->id ]) }}" class="btn btn-xs btn-danger">
+                                    Delete
+                                </a>
+                            </td>
+                        </tr>
+
+                    @endforeach
+                @else
+                    <tr>
+                        <th colspan="3" class="text-center">No Tags Created</th>
+                    </tr>
+                @endif
+            </tbody>
+        </table>
+
+    </div>
+
+</div>
+
+
+@stop
