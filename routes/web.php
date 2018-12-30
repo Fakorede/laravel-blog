5583f82356df3a589c58a73528a283d22d8663bd
fakorede <abiolafakorede@gmail.com>
2018-12-30 15:51:07 +0100

    created routes for tags

--- routes/web.php
+++ routes/web.php
@@ -72,6 +72,7 @@ Route::group(['prefix' => 'admin', 'middleware' => 'auth'], function() {
     'as' => 'post.update'
   ]);
 
+  // category routes
   Route::get('/category/create', [
     'uses' => 'CategoriesController@create',
     'as' => 'category.create'
@@ -100,6 +101,37 @@ Route::group(['prefix' => 'admin', 'middleware' => 'auth'], function() {
   Route::get('/category/delete/{id}', [
     'uses' => 'CategoriesController@destroy',
     'as' => 'category.delete'
+  ]);
+
+  // tag routes
+  Route::get('/tags', [
+    'uses' => 'TagsController@index',
+    'as' => 'tags'
+  ]);
+
+  Route::get('/tag/create', [
+    'uses' => 'TagsController@create',
+    'as' => 'tag.create'
+  ]);
+
+  Route::get('/tag/edit/{id}', [
+    'uses' => 'TagsController@edit',
+    'as' => 'tag.edit'
+  ]);
+
+  Route::post('/tag/store', [
+    'uses' => 'TagsController@store',
+    'as' => 'tag.store'
+  ]);
+
+  Route::post('/tag/update/{id}', [
+    'uses' => 'TagsController@update',
+    'as' => 'tag.update'
+  ]);
+
+  Route::get('/tag/delete/{id}', [
+    'uses' => 'TagsController@destroy',
+    'as' => 'tag.delete'
   ]);
 
 });
