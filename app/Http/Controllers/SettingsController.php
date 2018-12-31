1af43b102b9b8fe5c17ca823bd649234b9bf76fb
fakorede <abiolafakorede@gmail.com>
2018-12-31 21:31:47 +0100

    created settings controller

---
+++ app/Http/Controllers/SettingsController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Session;
+use App\Settings;
+use Illuminate\Http\Request;
+
+class SettingsController extends Controller
+{
+
+    public function _controller() {
+        $this->middleware('admin');
+    }
+
+    public function index() {
+        return view('admin.settings.settings')->with('settings', Settings::first());
+    }
+
+    public function update() {
+        $this->validate(request(), [
+            'site_name' => 'required',
+            'contact_number' => 'required',
+            'contact_email' => 'required',
+            'address' => 'required'
+        ]);
+
+        $settings = Settings::first();
+
+        $settings->site_name = request()->site_name;
+        $settings->contact_number = request()->contact_number;
+        $settings->contact_email = request()->contact_email;
+        $settings->address = request()->address;
+
+        $settings->save();
+
+        Session::flash('success', 'Settings Updated Successfully!');
+
+        return redirect()->back();
+    }
+}
