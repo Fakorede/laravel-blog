abbf75f1c76006573b4a7639fad5b7e7b79439bc
fakorede <abiolafakorede@gmail.com>
2019-01-01 17:21:45 +0100

    getting categories collection from the db

--- app/Http/Controllers/BlogController.php
+++ app/Http/Controllers/BlogController.php
@@ -16,7 +16,11 @@ class BlogController extends Controller
                 ->with('categories', Category::take(5)->get())
                 ->with('first_post', Post::orderBy('created_at', 'desc')->first())
                 ->with('second_post', Post::orderBy('created_at', 'desc')->skip(1)->take(1)->get()->first())
-                ->with('third_post', Post::orderBy('created_at', 'desc')->skip(2)->take(1)->get()->first());
+                ->with('third_post', Post::orderBy('created_at', 'desc')->skip(2)->take(1)->get()->first())
+                ->with('php', Category::find(2))
+                ->with('laravel', Category::find(3))
+                ->with('vue', Category::find(4))
+                ->with('settings', Settings::first());
     }
 }
 
