998438130616351a583e2a4d21afc262b746e6e1
fakorede <abiolafakorede@gmail.com>
2019-01-04 04:11:20 +0100

    installed addThis for sharing blog posts

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -68,56 +68,45 @@
                         </div>
                     </div>
 
-                    <div class="socials">Share:
-                        <a href="#" class="social__item">
-                            <i class="seoicon-social-facebook"></i>
-                        </a>
-                        <a href="#" class="social__item">
-                            <i class="seoicon-social-twitter"></i>
-                        </a>
-                        <a href="#" class="social__item">
-                            <i class="seoicon-social-linkedin"></i>
-                        </a>
-                        <a href="#" class="social__item">
-                            <i class="seoicon-social-google-plus"></i>
-                        </a>
-                        <a href="#" class="social__item">
-                            <i class="seoicon-social-pinterest"></i>
-                        </a>
-                    </div>
+                    
+                <!-- Share Post -->
+                <div class="addthis_inline_share_toolbox text-center"></div>
+            
 
                 </article>
 
                 <div class="blog-details-author">
 
                     <div class="blog-details-author-thumb">
-                        <img src="{{ asset('app/img/blog-details-author.png') }}" alt="Author">
+                        <img src="{{ asset($post->user->profile->avatar) }}" alt="Author">
                     </div>
 
                     <div class="blog-details-author-content">
                         <div class="author-info">
-                            <h5 class="author-name">Philip Demarco</h5>
-                            <p class="author-info">SEO Specialist</p>
+                        <h5 class="author-name">{{ $post->user->name }}</h5>
                         </div>
-                        <p class="text">Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam
-                            nonummy nibh euismod.
+                        <p class="text"> {{ $post->user->profile->about }}
                         </p>
                         <div class="socials">
 
-                            <a href="#" class="social__item">
-                                <img src="{{ asset('app/svg/circle-facebook.svg') }}" alt="facebook">
+                            <a href="{{ $post->user->profile->facebook }}" class="social__item" target="_blank">
+                                <img src="{{ asset('app/svg/facebook.svg') }}" alt="facebook">
                             </a>
 
-                            <a href="#" class="social__item">
+                            <a href="{{ $post->user->profile->twitter }}" class="social__item" target="_blank">
                                 <img src="{{ asset('app/svg/twitter.svg') }}" alt="twitter">
                             </a>
 
-                            <a href="#" class="social__item">
-                                <img src="{{ asset('app/svg/google.svg') }}" alt="google">
+                            <a href="{{ $post->user->profile->linkedin }}" class="social__item" target="_blank">
+                                <img src="{{ asset('app/svg/linkedin.svg') }}" alt="linkedin">
                             </a>
 
-                            <a href="#" class="social__item">
-                                <img src="{{ asset('app/svg/youtube.svg') }}" alt="youtube">
+                            <a href="{{ $post->user->profile->medium }}" class="social__item" target="_blank">
+                                <img src="{{ asset('app/svg/medium.svg') }}" alt="medium">
+                            </a>
+
+                            <a href="{{ $post->user->profile->github }}" class="social__item" target="_blank">
+                                <img src="{{ asset('app/svg/github.svg') }}" alt="github">
                             </a>
 
                         </div>
